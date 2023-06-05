830bdd9e1a0a89539fe0819c59a3ec0f7f0904ba
JT514569 <514569@chrome.escg.ac.uk>
2023-06-05 14:40:49 +0100

    Starting scripting.

---
+++ Assets/TTTCode.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class TTTCode : MonoBehaviour
+{
+    bool checker;
+    int plusone;
+
+    public Text BTNText1 = null;
+    public Text BTNText2 = null;
+    public Text BTNText3 = null;
+    public Text BTNText4 = null;
+    public Text BTNText5 = null;
+    public Text BTNText6 = null;
+    public Text BTNText7 = null;
+    public Text BTNText8 = null;
+    public Text BTNText9 = null;
+
+    public Text Reset = null;
+    public Text New = null;
+    public Text msgFeedback = null;
+
+    public Text PlayerX;
+    public Text PlayerO;
+
+    public Text PlayerP;
+}
